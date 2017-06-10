2868f27ef9a1a7f9554a73a0869e7f5282744e0a
yerenutku <yerenutku@gmail.com>
2017-06-10 16:45:16 +0300

    popup eklendi, yeni activity içerisine geçirildi

--- Uygulama1/app/src/main/java/com/erenutku/myapplication/MainActivity.java
+++ Uygulama1/app/src/main/java/com/erenutku/myapplication/MainActivity.java
@@ -1,29 +1,17 @@
 package com.erenutku.myapplication;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button btNewActivity;
     private EditText et1, et2;
     private String email = "gy@gmail.com", pass = "12345";
-    private String[] ulkeler =
-            {"Türkiye", "Almanya", "Avusturya", "Amerika","İngiltere",
-                    "Macaristan", "Yunanistan", "Rusya", "Suriye", "İran", "Irak",
-                    "Şili", "Brezilya", "Japonya", "Portekiz", "İspanya",
-                    "Makedonya", "Ukrayna", "İsviçre"};
-    private ListView lvList;
-    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,33 +32,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        lvList = (ListView) findViewById(R.id.lvList);
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,android.R.id.text1, ulkeler);
-        lvList.setAdapter(adapter);
-        lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
-                alertDialog.setMessage(ulkeler[position]);
-                alertDialog.setTitle("Seçilen Ülke");
-                alertDialog.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(MainActivity.this,"evete bastın",Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
-                        startActivity(intent);
-                    }
-                });
-                alertDialog.setNegativeButton("Hayır", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(MainActivity.this,"hayıra bastın",Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                alertDialog.show();
-            }
-        });
 
     }
 }
--- Uygulama1/app/src/main/java/com/erenutku/myapplication/SecondActivity.java
+++ Uygulama1/app/src/main/java/com/erenutku/myapplication/SecondActivity.java
@@ -1,13 +1,53 @@
 package com.erenutku.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+    private ListView lvList;
+    private ArrayAdapter<String> adapter;
+    private String[] ulkeler =
+            {"Türkiye", "Almanya", "Avusturya", "Amerika", "İngiltere",
+                    "Macaristan", "Yunanistan", "Rusya", "Suriye", "İran", "Irak",
+                    "Şili", "Brezilya", "Japonya", "Portekiz", "İspanya",
+                    "Makedonya", "Ukrayna", "İsviçre"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        lvList = (ListView) findViewById(R.id.lvList);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, ulkeler);
+        lvList.setAdapter(adapter);
+        lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(SecondActivity.this);
+                alertDialog.setMessage(ulkeler[position]);
+                alertDialog.setTitle(R.string.ulke_secimi);
+                alertDialog.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(SecondActivity.this, "evete bastın", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertDialog.setNegativeButton("Hayır", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(SecondActivity.this, "hayıra bastın", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertDialog.show();
+            }
+        });
     }
 }

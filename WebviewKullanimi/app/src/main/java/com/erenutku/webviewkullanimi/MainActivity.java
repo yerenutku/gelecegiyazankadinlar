0a79833add83e91413e516fca1ce762a1f38c2e1
yerenutku <yerenutku@gmail.com>
2017-06-11 15:54:21 +0300

    webview eklendi progress dialog eklendi ekstra kütüphane kullanıldı

--- WebviewKullanimi/app/src/main/java/com/erenutku/webviewkullanimi/MainActivity.java
+++ WebviewKullanimi/app/src/main/java/com/erenutku/webviewkullanimi/MainActivity.java
@@ -1,13 +1,39 @@
 package com.erenutku.webviewkullanimi;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ProgressDialog mProgressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.wvContent);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://gelecegiyazanlar.turkcell.com.tr");
+
+        mProgressDialog = ProgressDialog.
+                show(this,"Geleceği Yazan Kadınlar","Yükleniyor",true);
+        mProgressDialog.show();
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                mProgressDialog.dismiss();
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_SHORT);
+            }
+        });
     }
 }
